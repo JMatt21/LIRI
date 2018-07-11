5195eaf1c0b740b96adc9ae0934382d10db3083f
JustMatt <MatthewBertrandt@gmail.com>
2018-07-11 06:34:30 -0700

    Got twitter/spotify packages working

--- liri.js
+++ liri.js
@@ -1,15 +1,26 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-let request = require("request");
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+const keys = require("./keys.js");
+const request = require("request");
+const Spotify = require('node-spotify-api');
+const Twitter = require('twitter');
+const spotify = new Spotify(keys.spotify);
+const client = new Twitter(keys.twitter);
+
+var params = {screen_name: 'jimjon54025396'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets[1]);
+  }
+});
+
+
+
 
 switch (process.argv[2]) {
   case 'movie-this': {
     let movie_name;
     if (!process.argv[3]) {
       movie_name = 'Mr. Nobody';
-      console.log('setting movie_name to Mr.Nobody')
     }
     else {
       movie_name = process.argv[3];
@@ -39,7 +50,26 @@ switch (process.argv[2]) {
   };
     break;
   case 'spotify-this-song': {
+    let song_name;
+    if (!process.argv[3]) {
+      song_name = 'The Sign Ace';
+    }
+    else {
+      song_name = process.argv[3];
+    }
+    spotify.search({ type: 'track', query: song_name }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
 
+      let first_track = data.tracks.items[0];
+      // console.log(first_track);
+      console.log(first_track.artists[0].name);//need to loop over
+      console.log(first_track.name);
+      console.log(first_track.preview_url);
+      console.log(first_track.album.name);
+
+    });
   };
     break;
   case 'do-what-it-says': {
